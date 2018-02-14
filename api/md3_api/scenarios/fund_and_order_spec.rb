9e14f30d89e62da913f65ee1ac4ea23a11002bf3
Sam <sam.jeston@gmail.com>
2018-02-14 13:05:52 +1000

    Mega verbose logging to try understand the balance failure

--- api/md3_api/scenarios/fund_and_order_spec.rb
+++ api/md3_api/scenarios/fund_and_order_spec.rb
@@ -9,26 +9,61 @@ other_pb_for_trade = '1f1837e9-3d05-451c-9c6e-6bb921a72e47'
 
 def wait_for_full_settlement(account_id, order_id, order_direction, time_placed, order_amount)
   Helper::Order.wait_for_order_settlement(order_id, order_direction)
+
   deposit_amount = 0.0
   if order_direction == 'buy'
     cash_deposit = Helper::Transaction.find_cash_transaction_before_time(account_id, 'Cash Deposit', time_placed)[0]
     deposit_amount = cash_deposit.amount.to_f
   end
+
   loop do
     cash_withdrawal = Helper::Transaction.wait_on_cash_movement(account_id, 'Cash Withdrawal', time_placed)[0]
     withdraw_amount = cash_withdrawal.amount.to_f
-    puts "Deposit #{deposit_amount}"
-    puts "Order #{order_amount}"
-    puts "Withdraw #{withdraw_amount}"
+
     end_result = 0.0
     if order_direction == 'buy'
       end_result = deposit_amount - order_amount - withdraw_amount
     elsif order_direction == 'sell'
       end_result = order_amount - withdraw_amount
     end
-    puts "End Result #{end_result}"
+
     break if end_result == 0.0
     sleep 1
+  end
+end
+
+def print_out_balance_adjustments(pb_account_id, pc_account_id)
+  i = 1
+  loop do
+    break if i == 10
+
+    pb_balance = Db::AbxModules::Balance.where(accountId: pb_account_id, balanceTypeId: 'Account', marketplaceId: 1)
+    pb_bas = Db::AbxModules::BalanceAdjustment.where(balanceId: pb_balance[0].id)
+
+    puts "PB Balance Adjustments - Count #{pb_bas.length} #####################################"
+    pb_js = pb_bas.as_json
+    pb_readable = pb_js.map do |x|
+      x['amount'] = x['amount'].to_f
+      x
+    end
+    puts pb_readable
+    puts '############################################################'
+
+    pc_balance = Db::AbxModules::Balance.where(accountId: pc_account_id, balanceTypeId: 'Account', marketplaceId: 1)
+    pc_bas = Db::AbxModules::BalanceAdjustment.where(balanceId: pc_balance[0].id)
+
+    puts "PC Balance Adjustments  - Count #{pc_bas.length}#####################################"
+    pc_js = pc_bas.as_json
+    pc_readable = pc_js.map do |x|
+      x['amount'] = x['amount'].to_f
+      x
+    end
+    puts pc_readable
+    puts '############################################################'
+
+    i += 1
+
+    sleep 5
   end
 end
 
@@ -40,7 +75,7 @@ describe 'Fund And Order' do
     expect(res[:data]['errors'][0]['detail']).to eq('There is no depth for this order to match against')
   end
 
-  it 'can correctly Md3ApiHttphit multiple levels of depth' do
+  it 'can correctly hit multiple levels of depth' do
     r1 = Md3ApiHttp.place_order(pb_uuid, pb_uuid, contract_id, 'limit', 'sell', 5, 100, 'GTC')
     expect(r1[:code]).to eq(201)
 
@@ -58,8 +93,11 @@ describe 'Fund And Order' do
     expect(res[:data]['data'][1]['attributes']['accountId']).to eq(pc_uuid)
     expect(res[:data]['data'][1]['attributes']['quantity']).to eq(5)
 
-    # Allow for settlement
-    sleep 1
+    order_id = res[:data]['data'][0]['attributes']['orderId']
+    order_placed_at = res[:data]['data'][0]['attributes']['createdAt']
+    settlement_amount = res[:data]['data'].map { |row| row['attributes']['settlementAmount'] }.reduce(:+)
+
+    wait_for_full_settlement(pc_uuid, order_id, 'buy', order_placed_at, settlement_amount)
   end
 
   # In this scenario we expect that the clients trade balance will be the same at the
@@ -84,10 +122,11 @@ describe 'Fund And Order' do
     # Allow for settlement
     wait_for_full_settlement(pc_uuid, order_id, 'buy', order_placed_at, settlement_amount)
 
-    sleep 30
     pb_account_after_trade = Helper::Balance.find_account_balance(pb_uuid, 1)
     pc_account_after_trade = Helper::Balance.find_account_balance(pc_uuid, 1)
     pc_holdings_after_trade = Helper::Balance.find_holdings_balance(pc_uuid, contract_id.to_i)
+
+    print_out_balance_adjustments(pb_uuid, pc_uuid)
 
     expect(pc_account_before_trade.to_f).to eq(pc_account_after_trade.to_f)
     expect(pc_holdings_before_trade + 5).to eq(pc_holdings_after_trade)
@@ -117,10 +156,11 @@ describe 'Fund And Order' do
     # Allow for settlement. Not sure why this settlement is so slow
     wait_for_full_settlement(pc_uuid, order_id, 'sell', order_placed_at, settlement_amount)
 
-    sleep 30
     pb_account_after_trade = Helper::Balance.find_account_balance(pb_uuid, 1)
     pc_account_after_trade = Helper::Balance.find_account_balance(pc_uuid, 1)
     pc_holdings_after_trade = Helper::Balance.find_holdings_balance(pc_uuid, contract_id.to_i)
+
+    print_out_balance_adjustments(pb_uuid, pc_uuid)
 
     expect(pc_holdings_before_trade - 5).to eq(pc_holdings_after_trade)
     expect(pc_account_before_trade.to_f).to eq(pc_account_after_trade.to_f)
