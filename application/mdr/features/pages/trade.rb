f75b180228c663e4733dd19184857475c8d7df36
Unknown <anita.coffey@abx.com>
2018-08-31 15:19:42 +1000

    WIP

--- application/mdr/features/pages/trade.rb
+++ application/mdr/features/pages/trade.rb
@@ -2,16 +2,12 @@
 class TradePage
   def initialize
     @trade_button = $browser.divs(class: 'navbar-start')[0].as(class: 'navbar-item')[0].click()
-
-  #  @cash_transfers_button = $browser.divs(class: 'navbar-start')[0].as(class: 'navbar-item')[1].click()
-  #  @settings_button = $browser.divs(class: 'navbar-start')[0].as(class: 'navbar-item')[2].click()
-  #  @reports = $browser.divs(class: 'navbar-start')[0].as(class: 'navbar-item')[3].click()
     @Trade_Gold = $browser.divs(class: 'tabs is-centered')[0].link(text: 'Gold')
     @Trade_Silver = $browser.divs(class: 'tabs is-centered')[0].link(text: 'Silver')
     @Trade_Platinum = $browser.divs(class: 'tabs is-centered')[0].link(text: 'Platinum')
 
-    @buy_first_metal = $browser.as(class: 'button is-success')[0].href
-    @sell_first_metal = $browser.as(class: 'button is-danger')[0].href
+    @buy_first_metal = $browser.ps(text: 'Buy')[0]
+    @sell_first_metal = $browser.ps(text: 'Sell')[0]
 
     @dubai_hub = $browser.divs(class: 'control')[1].option(value: "32")
     @hongKong_hub = $browser.divs(class: 'control')[1].option(value: "23")
@@ -21,9 +17,9 @@ class TradePage
     @sydney_hub = $browser.divs(class: 'control')[1].option(value: "25")
     @zurich_hub = $browser.divs(class: 'control')[1].option(value: "29")
 
-    @order_quantity = $browser.td(class: 'input')[0].input(name: 'amount')
+    @order_quantity = $browser.input(name: 'amount')
 
-    @confirm_button = $browser.nuttons(class: 'button is-success')[1].buttons(text: 'Confirm')[0]
+    @confirm_button = $browser.buttons(text: 'Confirm')[0]
 
   #   @order_panel = $browser.div(class: 'column is-half')
   #   @market_order_button = @order_panel.button(text: 'Market')
@@ -33,16 +29,6 @@ class TradePage
   #   @confirm_button = $browser.div(text: 'button is-success').find_element(text: "Confirm")
   #   @order_quantity_control = $browser.td(name: 'amount').text_field
 
-
-    # @left_panel_headers = $browser.div(class: ['column-headers', 'left'])
-    # @left_panel_contracts = $browser.div(class: 'left-col')
-    # @left_contract_blocks = @left_panel_contracts.divs(class: 'product-datablock-summary-container')
-    # @right_panel = $browser.div(class: ['column-headers', 'right'])
-    # @left_filter_toggle = @left_panel_headers.link(class: 'filterToggle')
-    # @left_product_filters = @left_panel_headers.div(class: ['product-filters', 'active'])
-    # @left_mode_selector = @left_panel_headers.select_list(class: 'market-selector')
-    # @left_hub_selector = @left_panel_headers.select_list(class: 'hub-selector')
-    # @left_metal_selector = @left_panel_headers.select_list(class: 'metal-selector')
     # @buy_button = $browser.link(class: 'buy')
     # @sell_button = $browser.link(class: 'sell')
     # @market_order_button = $browser.link(class: 'orderForm__orderType--market')
@@ -53,54 +39,49 @@ class TradePage
     # @submit_order_button = $browser.link(text: 'Submit Order')
   end
 
-  def insert_quantity(quantity)
-    @order_quantity.click()
-    @order_quantity.send_keys(quantity)
-  end
+  # def top_sell_depth
+  #   top_sell_depth_text = $browser.div(class: 'product-bid-offer').dl(class: 'bid').dd.link.text
+  #   sell_depth_without_comma = top_sell_depth_text
+  #                              .chars
+  #                              .reject { |i| i == ',' }
+  #                              .join
 
-  def top_sell_depth
-    top_sell_depth_text = $browser.div(class: 'product-bid-offer').dl(class: 'bid').dd.link.text
-    sell_depth_without_comma = top_sell_depth_text
-                               .chars
-                               .reject { |i| i == ',' }
-                               .join
+  #   sell_depth_without_comma.to_f
+  # end
 
-    sell_depth_without_comma.to_f
-  end
+  # def top_buy_depth
+  #   top_buy_depth_text = $browser.div(class: 'product-bid-offer').dl(class: 'offer').link(class: 'buy').text
+  #   buy_depth_without_comma = top_buy_depth_text
+  #                             .chars
+  #                             .reject { |i| i == ',' }
+  #                             .join
 
-  def top_buy_depth
-    top_buy_depth_text = $browser.div(class: 'product-bid-offer').dl(class: 'offer').link(class: 'buy').text
-    buy_depth_without_comma = top_buy_depth_text
-                              .chars
-                              .reject { |i| i == ',' }
-                              .join
+  #   buy_depth_without_comma.to_f
+  # end
 
-    buy_depth_without_comma.to_f
-  end
+  # def set_order_price(direction, distance)
+  #   elements = TradePage.new
+  #   top_of_depth = if direction == 'buy'
+  #                    elements.top_buy_depth
+  #                  else
+  #                    elements.top_sell_depth
+  # end
+  #   direction == 'buy' ? top_of_depth - distance : top_of_depth + distance
+  # end
 
-  def set_order_price(direction, distance)
-    elements = TradePage.new
-    top_of_depth = if direction == 'buy'
-                     elements.top_buy_depth
-                   else
-                     elements.top_sell_depth
-  end
-    direction == 'buy' ? top_of_depth - distance : top_of_depth + distance
-  end
+  # def select_client_filter(client_hin)
+  #   $browser.input(class: 'clientSelect__filterInput').click
+  #   $browser.span(text: Regexp.new(client_hin)).click
+  # end
 
-  def select_client_filter(client_hin)
-    $browser.input(class: 'clientSelect__filterInput').click
-    $browser.span(text: Regexp.new(client_hin)).click
-  end
-
-  def select_date_time_in_future
-    $browser.link(class: 'orderForm__orderValidity--gtd').click
-    $browser.fieldset(class: 'bc-group datetime-combo').click
-    $browser.div(class: 'picker__nav--next').click
-    $browser.div(class: 'picker__box').click
-    $browser.fieldset(class: 'bc-group datetime-combo').input(class: 'timepicker').click
-    $browser.ul(class: 'picker__list').li(index: 2).click
-  end
+  # def select_date_time_in_future
+  #   $browser.link(class: 'orderForm__orderValidity--gtd').click
+  #   $browser.fieldset(class: 'bc-group datetime-combo').click
+  #   $browser.div(class: 'picker__nav--next').click
+  #   $browser.div(class: 'picker__box').click
+  #   $browser.fieldset(class: 'bc-group datetime-combo').input(class: 'timepicker').click
+  #   $browser.ul(class: 'picker__list').li(index: 2).click
+  # end
 
   attr_reader :order_price_control
 
@@ -127,6 +108,10 @@ class TradePage
   attr_reader :buy_first_metal
 
   attr_reader :sell_first_metal
+
+  attr_reader :order_quantity
+
+  attr_reader :confirm_button
 
   attr_reader :left_panel_headers
 
--- application/mdr/features/step_definitions/trade.rb
+++ application/mdr/features/step_definitions/trade.rb
@@ -37,54 +37,26 @@ And(
 end
 end
 
-And('I place a {string} market order') do |direction|
+And('I place a {string} market order for a quantity of {int}') do |direction, quantity|
   elements = TradePage.new
-  byebug
 
   if direction == 'buy'
-    elements.buy_first_metal.click()
+    elements.buy_first_metal.click
   elsif direction == 'sell'
-    elements.sell_first_metal.click()
-end
+    elements.sell_first_metal.click
 end
 
-And(
-  'I set a quantity of {int}'
-) do |quantity|
-elements = TradePage.new
-elements.insert_quantity(quantity)
-# elements.order_quantity_control.set(quantity)
+elements.order_quantity.click()
+elements.order_quantity.send_keys(quantity)
 elements.confirm_button.click()
 end
 
-And(
-  'I place a {string} limit order in the selected contract for a quantity of {int} '\
-  'at a price {int} away from the top of the depth'
-) do |direction, quantity, distance|
-  elements = TradePage.new
-
-  if direction == 'buy'
-    elements.buy_button.click
-  else
-    elements.sell_button.click
-  end
-
-  # Animations are the worst, this sleep awaits the panel to pop completely
-  sleep 1
-
-  elements.limit_order_button.click
-  elements.order_quantity_control.set(quantity)
-  order_price = elements.set_order_price(direction, distance)
-  elements.order_price_control.set(order_price)
-  elements.review_order_button.click
-  elements.submit_order_button.click
-end
 
 And(
   'I validate the matched order in the database '\
   'with order details {int}, {string}, {int}, {string} for the user {string}'
-) do |contract_id, direction, quantity, order_type, user_set|
-  user = YamlLoader.user_info(user_set)
+) do |contract_id, direction, quantity, order_type, data_set|
+  user = YamlLoader.user_info(data_set)
   username = user['username']
 
   # Give the immediate settlement service 2 seconds to create trade transactions for the order
@@ -109,70 +81,45 @@ And(
   order_matches = Helper::Order.find_order_matches(order.id, direction)
   trade_transactions = order_matches.map do |order_match|
     Helper::Transaction.find_trade_transaction(order_match.id, account_uuid)
+    byebug
   end
 
-  recorded_quantity = trade_transactions.map(&:quantity)
-  unless recorded_quantity[0] == quantity.to_i
-    raise 'The quantity accumlated in the trade transactions does not equal the test condition'
-  end
+  # recorded_quantity = trade_transactions.map(&:quantity)
+  # unless recorded_quantity[0] == quantity.to_i
+  #   raise 'The quantity accumlated in the trade transactions does not equal the test condition'
+  # end
 
-  trade_transactions.each do |tt|
-    recorded_cost = tt.settlementAmount
-    account_balance_movement = Helper::Transaction.account_balance_change_with_trade_transaction(
-      tt.id,
-      account_uuid,
-      direction,
-      2
-    )
+  # trade_transactions.each do |tt|
+  #   recorded_cost = tt.settlementAmount
+  #   account_balance_movement = Helper::Transaction.account_balance_change_with_trade_transaction(
+  #     tt.id,
+  #     account_uuid,
+  #     direction,
+  #     2
+  #   )
 
-    qty = tt.quantity
-    holdings_balance_movement = Helper::Transaction.holdings_balance_change_with_trade_transaction(
-      tt.id,
-      account_uuid,
-      direction,
-      contract_id
-    )
+  #   qty = tt.quantity
+  #   holdings_balance_movement = Helper::Transaction.holdings_balance_change_with_trade_transaction(
+  #     tt.id,
+  #     account_uuid,
+  #     direction,
+  #     contract_id
+  #   )
 
     # We invert the cost if it is a buy order as the account balance should go down
     # We invert the qty if it is a sell order as the holdings balance should go down
-    if direction == 'buy'
-      recorded_cost = -recorded_cost
-    else
-      qty = -qty
-    end
+    # if direction == 'buy'
+    #   recorded_cost = -recorded_cost
+    # else
+    #   qty = -qty
+    # end
 
-    unless recorded_cost == account_balance_movement
-      raise 'Trade transaction settlement amount does not match balance movement'
-    end
+    # unless recorded_cost == account_balance_movement
+    #   raise 'Trade transaction settlement amount does not match balance movement'
+    # end
 
-    unless qty == holdings_balance_movement
-      raise 'Trade transaction quantity amount does not match balance movement'
-    end
-  end
-end
-
-And(
-  'I validate the open order in the database '\
-  'with order details {int}, {string}, {int}, {string} for the user {string} and order validity {string}'
-) do |contract_id, direction, quantity, order_type, user_set, order_validity |
-  user = YamlLoader.user_info(user_set)
-  username = user['username']
-
-  # Give the immediate settlement service 2 seconds to create trade transactions for the order
-  sleep 2
-
-  account_uuid = Helper::Account.find_account_uuid(username)
-  order = Helper::Order.find_order(account_uuid, contract_id, direction, quantity)
-
-  raise 'The order is not open' unless order.status == 'submit'
-  raise 'The order was of the wrong type' unless order.orderType == order_type
-  raise 'The order was of the wrong validity' unless order.validity == order_validity
-
-  # Ensure this is the correct order by making sure it occured in the last 20 seconds
-  order_created = order.createdAt.to_i
-  current_time = Time.now.to_i
-
-  unless order_created.between?(current_time - 20, current_time + 20)
-    raise 'The orders timestamp is incorrect'
-  end
+    # unless qty == holdings_balance_movement
+    #   raise 'Trade transaction quantity amount does not match balance movement'
+    # end
+  # end
 end
